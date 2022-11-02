3735ce674451ac595832aa2a004c784403d9a53c
Luo Shi Fa <1246449573@qq.com>
2022-11-02 16:56:24 +0800

    20221102v2

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="wrapper">
+    <music></music>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:"app",
+    data(){
+      return{
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  #wrapper {
+    width: 100%;
+    height: 100%;
+    background-color:#e5e6c4;
+  }
+</style>
